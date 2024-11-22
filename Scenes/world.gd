0f220f776db3eb2438411aef39acb65157a3d8fe
UnmotivatedArtist <Thomas.lewis.ross@gmail.com>
2024-11-22 10:58:32 -0600

    The United Bank of M O N E Y

--- Scenes/world.gd
+++ Scenes/world.gd
@@ -1,3 +1,9 @@
 extends Control
+var money = 0
+
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+
+func _on_money_gen_pressed() -> void:
+	money = money + 1000
+	
